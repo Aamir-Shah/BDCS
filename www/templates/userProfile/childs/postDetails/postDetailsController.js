d68e68df9e02e5eef6e8499cf31299e6b7d9251f
aamir-shah <syedaamir974@gmail.com>
2016-04-22 02:55:45 -0700

    validation added on login form and comment section added

--- www/templates/userProfile/childs/postDetails/postDetailsController.js
+++ www/templates/userProfile/childs/postDetails/postDetailsController.js
@@ -1,11 +1,25 @@
 angular
     .module('starter')
 
-    .controller('postDetailsController', function($scope, $stateParams, $firebaseRef, $firebaseObject) {
-        console.log($stateParams.uid)
-        console.log($stateParams.postUid)
+    .controller('postDetailsController', function ($scope, userData, $stateParams, $firebaseRef, $firebaseObject, $firebaseArray) {
 
         $scope.postDetail = $firebaseObject($firebaseRef.requests.child($stateParams.uid).child($stateParams.postUid))
         console.log($scope.postDetail)
+        $scope.localStorageData = userData.localStorage();
+
+        $scope.comment = {
+            uid: $scope.localStorageData.uid,
+            time: Firebase.ServerValue.TIMESTAMP,
+            profilePic: $scope.localStorageData.password.profileImageURL
+        }
+
+        $scope.allComments = $firebaseArray($firebaseRef.requests.child($stateParams.uid).child($stateParams.postUid).child('postComments'));
+        console.log($scope.allComments);
+
+        $scope.doComment = function (comment) {
+            $firebaseArray($firebaseRef.requests.child($stateParams.uid).child($stateParams.postUid).child('postComments')).$add(comment);
+            console.log(arguments);
+            comment.commentText = undefined;
+        }
 
     })
